c8bd97469ef550a8b7fd644b46ae1ec11d4f3702
= <=>
2017-12-17 15:52:56 -0200

    --

---
+++ chap3-Functions.js
@@ -0,0 +1,28 @@
+
+//playing with strings:
+var name = String(prompt("name: "));
+var country = String(prompt("country: "));
+console.log(`Hello, ${name} from ${country}.\nHope you are fine.`);
+
+
+//playing with arguments object
+var sumItAll = function (){
+  var result = 0;
+  for(let i = 0; i < arguments.length; i++){
+    if(isFinite(arguments[i])) result += arguments[i];
+  }
+  return result;
+}
+function randInteger(){
+  return ( Math.round(Math.random() *1000) % 100 ) + 1;
+}
+
+function randomIntegerArray(){
+  var array = [];
+  for (let i = 0; i < randInteger(); i++){
+    array[i] = randInteger();
+  }
+  return array;
+}
+// a little bit of ... operator enhances by ES6
+console.log(sumItAll(...randomIntegerArray()));
